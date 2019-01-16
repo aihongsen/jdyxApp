5696b2e005101229358906e0664d5644a70bbbb1
carldevin <carldevin@126.com>
2019-01-16 17:15:22 +0800

    1.16修改

---
+++ app-video/src/main/java/com/jdyx/app/appvideo/service/impl/ProvincialServiceImpl.java
@@ -0,0 +1,9 @@
+package com.jdyx.app.appvideo.service.impl;
+
+import com.jdyx.app.service.ProvincialService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProvincialServiceImpl implements ProvincialService {
+
+}
